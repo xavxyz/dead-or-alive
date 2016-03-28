2824fbc280de38dbd21482b28d31d87563982fd2
xavizalote <xavier.cazalot@gmail.com>
2016-03-28 17:28:15 +0200

    create splash

---
+++ client/components/Splash.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default Splash = () => {
+	return (
+		<div className="ui container" style={{ marginTop: '10%' }}>
+			<div className="ui stackable two column centered grid">
+				<div className="column row">
+					<h1 className="ui huge header">Dead or Alive</h1>
+				</div>
+				<div className="column row">
+					<img className="ui image rounded" src="http://i.giphy.com/iaUwUWwY99Tuo.gif" />
+				</div>
+				<div className="column row">
+					<h2 className="ui center aligned header">Does your startup generates revenue?</h2>
+				</div>
+				<div className="column row">
+					<div className="ui buttons">
+						<button className="ui positive button">YES</button>
+						<div className="or"></div>
+						<button className="ui negative button">NO</button>
+					</div>
+				</div>
+				<div className="ui stackable two column grid">
+					<div className="column">
+						<h3 className="ui header center aligned">
+							<img className="ui mini circular image" src="https://cdn.ycombinator.com/images/people/paulg-3f2f4e41.jpg" />
+							<div className="content">
+								Few do it for the money. Fewer do it without the money.
+								<div className="sub header">Paul Graham</div>
+							</div>
+						</h3>
+					</div>
+					<div className="column">
+						<h3 className="ui header center aligned">
+							<img className="ui mini circular image" src="https://cdn-images-1.medium.com/fit/c/60/60/0*SSaXMiDnoQL-MBt6.jpeg" />
+							<div className="content">
+								For 99% of companies, the life metrics come down to revenue.
+								<div className="sub header">Oussama Ammar</div>
+							</div>
+						</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
