cc0e103e74cad0373fecdd87995488e0aa27d7f1
xavizalote <xavier.cazalot@gmail.com>
2016-03-30 16:33:39 +0200

    ESLindt: wrapped chocolat for easter :rabbit:

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { moment } from 'meteor/momentjs:moment';
 import Revenues from '../lib/collections';
 
-Meteor.publish('revenues.all', function () {
-	return Revenues.find({userId: this.userId, createdAt: {$gte: moment().subtract(3, 'months').toDate()}});
+Meteor.publish('revenues.all', function userRevenues() {
+	return Revenues.find(
+		{ userId: this.userId,
+			createdAt: { $gte: moment().subtract(3, 'months').toDate() },
+		}
+	);
 });
